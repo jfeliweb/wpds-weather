fff2b46b423b358177b0546f47672eb1e9e1b179
Nicolas Perrenoud <mail@nicu.ch>
2016-06-13 14:13:28 +0200

    CSS cleanup

--- weather.js
+++ weather.js
@@ -5,7 +5,7 @@ jQuery(document).ready(function ($) {
 			location: elem.attr('data-place'),
 			unit: elem.attr('data-unit'),
 			success: function(weather) {
-				html = '<i class="icon weather-'+weather.code+'"></i><span class="temperature">'+weather.temp+'&deg;'+weather.units.temp+'<br /><span class="condition">'+weather.currently+'</span>';
+				html = '<i class="icon weather-'+weather.code+'"></i><span class="temperature">'+weather.temp+'&deg;'+weather.units.temp+'<span class="condition">'+weather.currently+'</span>';
 				elem.html(html);
 			},
 			error: function(error) {
